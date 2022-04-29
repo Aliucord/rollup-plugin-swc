7bddb29312c8f9c0012223bc714b6d825a8ea1a8
js6pak <kubastaron@hotmail.com>
2022-04-29 18:07:24 +0200

    Port resolveId from exuanbo/rollup-plugin-esbuild-transform

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {Options, transform} from '@swc/core'
 import {Plugin} from 'rollup'
 import {createFilter, FilterPattern} from '@rollup/pluginutils'
+import { dirname, isAbsolute, join, resolve } from 'path'
+import { promises as fs } from 'fs'
 
 type SWCPluginOptions<O = Options> = Pick<O, Exclude<keyof O, 'filename'>>
 
@@ -15,6 +17,19 @@ type PluginOptions = SWCPluginOptions & RollupOptions
 
 type RollupPluginSWC = (options?: PluginOptions) => Plugin
 
+const resolveFilename = async (
+  basename: string
+): Promise<string | null> => {
+  for (const extension of ['js', 'jsx', 'ts', 'tsx']) {
+    const possibleFilename = `${basename}.${extension}`
+    try {
+      await fs.access(possibleFilename)
+      return possibleFilename
+    } catch {}
+  }
+  return null
+}
+
 const swc: RollupPluginSWC = (pluginOptions = {}) => {
   const {rollup, ...options} = pluginOptions
 
@@ -22,6 +37,19 @@ const swc: RollupPluginSWC = (pluginOptions = {}) => {
 
   return {
     name: 'swc',
+    async resolveId(source, importer) {
+      if (importer === undefined || !(source.startsWith('.') || isAbsolute(source))) {
+        return null
+      }
+      const filename = resolve(dirname(importer), source)
+      try {
+        return (await fs.stat(filename)).isDirectory()
+          ? await resolveFilename(join(filename, 'index'))
+          : filename
+      } catch {
+        return await resolveFilename(filename)
+      }
+    },
     transform(code, filename) {
       if (!filter(filename)) {
         return null
